233546bb4bb15a831a18aa89e3292753c9b6adea
Vojtech Vondra <v.vondra@foodpanda.com>
2016-11-11 20:55:01 +0100

    Verify update existing function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 exports.myHandler = function(event, context, callback) {
    console.log("value1 = " + event.key1);
    console.log("value2 = " + event.key2);  
-   console.log("value3 = " + event.key3);
    callback(null, "some success message");
 }
