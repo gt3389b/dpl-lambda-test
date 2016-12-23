1fbb9620c2510787157163b030323e6a7ee2f8a7
Vojtech Vondra <v.vondra@foodpanda.com>
2016-12-23 22:01:49 +0100

    Test create new function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.myHandler = function(event, context, callback) {
    console.log("value1 = " + event.key1);
    console.log("value2 = " + event.key2);  
+   console.log("value3 = " + event.key3);
    callback(null, "some success message");
 }
