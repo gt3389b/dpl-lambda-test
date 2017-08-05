f745bcb0d8aa20924498ca152f827a5aa43fcc36
Russell Leake <gt3389b@gmail.com>
2017-08-05 08:07:30 -0400

    Testing uuid

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const uuidv5 = require('uuid/v5');
 exports.myHandler = function(event, context, callback) {
    if (event.key1) {
       console.log("value1 = " + event.key1);
-      callback(null, uuidv5(event.key1, uuidv5()));
+      callback(null, uuidv5("Hello world", uuidv5()));
       }
    else {
       callback(null, "need key1 in request body");
